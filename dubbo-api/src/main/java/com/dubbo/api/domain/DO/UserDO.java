4ba7e84b426ec5e82e94058c45facd876f2b24a8
tedburner <jlj_vip98@qq.com>
2019-08-12 15:11:02 +0800

    dubbo 修改框架

--- dubbo-api/src/main/java/com/dubbo/api/domain/DO/UserDO.java
+++ dubbo-api/src/main/java/com/dubbo/api/domain/DO/UserDO.java
@@ -1,13 +1,16 @@
 package com.dubbo.api.domain.DO;
 
+import lombok.Data;
+
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author: Lucifer
  * @date: 2018/11/19 21:23
  * @description:
  */
+@Data
 public class UserDO implements Serializable {
 
     private Long id;
@@ -17,94 +20,6 @@ public class UserDO implements Serializable {
     private String card;
     private String phone;
     private Integer status;
-    private LocalDateTime createTime;
-    private LocalDateTime updateTime;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getCard() {
-        return card;
-    }
-
-    public void setCard(String card) {
-        this.card = card;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDO{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", card='" + card + '\'' +
-                ", phone='" + phone + '\'' +
-                ", status=" + status +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
-    }
+    private Date createTime;
+    private Date updateTime;
 }
